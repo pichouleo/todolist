3bfaec439cec214e52b76ef4cf722f537b9842d8
Leo <leo.pichou@sfr.fr>
2023-05-22 12:10:45 +0200

    0.1.0
    -Ajout status tâche

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,23 +2,31 @@
 
 @section('content')
 <div class="relative overflow-x-auto text-center bg-ffe194">
-    <div style="width:300px;margin: 0 auto;">
-        <x-application-logo/>   
-    </div>
-    <p class="text-center text-gray-500" style="margin:-20px 0 10px 0;">Bienvenue sur notre nouvelle liste de tâches interne !</p>
-    <x-primary-button onclick="window.location.href = '{{ route('tasks.create') }}'" style="margin:10px 0; background-color:#09600F;">
+    <h1 class="font-black text-6xl font-carter-one">MyToTool</h1>
+    <p class="text-gray-500 my-3">Bienvenue sur notre nouvelle liste de tâches interne !</p>
+    <x-primary-button onclick="window.location.href = '{{ route('tasks.create') }}'" class="mb-3">
     Ajouter une tâche
     </x-primary-button>
-    <ul style="width:50%; margin: 5px auto;">
+    <ul class="w-1/2 mx-auto">
         @foreach ($userTasks as $task)
-            <li class="bg-white border-b rounded-lg">
+            <li class="bg-white border-b rounded-lg mb-2">
                 <div class="flex items-center justify-between px-6 py-4">
-                    <div class="whitespace-nowrap">
+                    <div class="text-gray-700">
                         {{ $task->name }}
                     </div>
                     <div class="flex flex-row items-center">
-                    <a href="{{ route('tasks.edit', $task->id) }}" style="margin-right:5px;">Modifier</a>
-                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline-block">
+                        <div class="flex items-center text-gray-500 text-sm mr-8">
+                            @if($task->status === 'En cours')
+                                <div class="w-3 h-3 bg-amber-200 rounded-full mx-2" ></div>
+                            @elseif($task->status === 'Urgent')
+                                <div class="w-3 h-3 bg-red-700 rounded-full mx-2"></div>
+                            @elseif($task->status === 'Terminé')
+                            <div class="w-3 h-3 bg-green-600 rounded-full mx-2"></div>
+                            @endif
+                            {{ $task->status }}
+                        </div>
+                        <a href="{{ route('tasks.edit', $task->id) }}"><i class="fas fa-pen"></i></a>
+                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline-block ml-3">
                             @csrf
                             @method('DELETE')
                             <x-danger-button>
