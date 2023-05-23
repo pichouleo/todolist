4e8662941a28cdbe92228a7e3f811860707db4c6
Leo <leo.pichou@sfr.fr>
2023-05-23 16:05:29 +0200

    0.2.0
    -simplification view :
    suppression edit et create view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -22,48 +22,69 @@
     <h1 class="font-black text-3xl font-carter-one text-center mb-5">Ma liste de tâches</h1>
     <ul class="w-1/2 mx-auto">
         @foreach ($userTasks as $task)
-            <li class="bg-white border-b rounded-lg mb-2 shadow-sm">
-                <div class="flex items-center justify-between px-6 py-4">
-                    <div class="text-gray-700">
-                        {{ $task->name }}
-                    </div>
-                    <div class="flex flex-row items-center">
-                        <div class="flex items-center text-gray-500 text-sm mr-8">
-                            @if($task->status === 'En cours')
-                                <div class="w-3 h-3 bg-amber-200 rounded-full mx-2" ></div>
-                            @elseif($task->status === 'Urgent')
-                                <div class="w-3 h-3 bg-red-700 rounded-full mx-2"></div>
-                            @elseif($task->status === 'Terminé')
+        <li id="taskLi-{{ $task->id }}" class="bg-white border-b rounded-lg mb-2 shadow-sm">
+            <div class="flex items-center justify-between px-6 py-4">
+                <div class="text-gray-700">
+                    {{ $task->name }}
+                </div>
+                <div class="flex flex-row items-center">
+                    <div class="flex items-center text-gray-500 text-sm mr-8">
+                        @if($task->status === 'En cours')
+                            <div class="w-3 h-3 bg-amber-200 rounded-full mx-2"></div>
+                        @elseif($task->status === 'Urgent')
+                            <div class="w-3 h-3 bg-red-700 rounded-full mx-2"></div>
+                        @elseif($task->status === 'Terminé')
                             <div class="w-3 h-3 bg-green-600 rounded-full mx-2"></div>
-                            @endif
-                            {{ $task->status }}
-                        </div>
-                        <a href="{{ route('tasks.edit', $task->id) }}"><i class="fas fa-pen"></i></a>
-                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline-block ml-3">
-                            @csrf
-                            @method('DELETE')
-                            <x-danger-button>
-                                Supprimer
-                            </x-danger-button>
-                        </form>
+                        @endif
+                        {{ $task->status }}
                     </div>
+                    <a href="#" onclick="toggleTaskEditForm({{ $task->id }})">
+                        <i class="fas fa-pen"></i>
+                    </a>
+                    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline-block ml-3">
+                        @csrf
+                        @method('DELETE')
+                        <x-danger-button>Supprimer</x-danger-button>
+                    </form>
                 </div>
-            </li>
-            <form action="{{ route('tasks.update', $task->id) }}" method="POST" class="flex items-center justify-between mx-auto bg-white border-b rounded-lg max-w-1/2 px-6 py-4">
-                @csrf
-                @method('PUT')
+            </div>
+        </li>
 
-                <div class=" form-group">
-                    <input type="text" name="name" id="name" value="{{ $task->name }}"  class="form-control border-gray-300 border rounded-md">
-                </div>
-                <select name="status"  class="bg-white border-gray-300 border rounded-md py-2 px-3 pr-8 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+        <form id="editForm-{{ $task->id }}" action="{{ route('tasks.update', $task->id) }}" method="POST" class="flex items-center justify-between mx-auto bg-white border-b rounded-lg max-w-1/2 px-6 py-3 mb-2 hidden">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <input type="text" name="name" id="name" value="{{ $task->name }}" class="form-control border-gray-300 border rounded-md">
+            </div>
+            <div class="flex flex-row items-center">
+                <select name="status" class="bg-white border-gray-300 border rounded-md py-2 px-3 pr-8 mr-8 leading-tight focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent text-sm">
                     <option value="En cours" {{ $task->status === 'En cours' ? 'selected' : '' }}>En cours</option>
                     <option value="Terminé" {{ $task->status === 'Terminé' ? 'selected' : '' }}>Terminé</option>
                     <option value="Urgent" {{ $task->status === 'Urgent' ? 'selected' : '' }}>Urgent</option>
                 </select>
+                <button type="button" onclick="cancelTaskEditForm({{ $task->id }})"><i class="fas fa-times text-2xl"></i></button>
+                <x-primary-button class="ml-3">Modifier</x-primary-button>
+            </div>
+        </form>
 
-                <x-primary-button>Modifier</x-primary-button>
-            </form>
+        <script>
+            function toggleTaskEditForm(taskId) {
+                const taskLi = document.getElementById('taskLi-' + taskId);
+                const editForm = document.getElementById('editForm-' + taskId);
+
+                taskLi.classList.toggle('hidden');
+                editForm.classList.toggle('hidden');
+            }
+
+            function cancelTaskEditForm(taskId) {
+                const taskLi = document.getElementById('taskLi-' + taskId);
+                const editForm = document.getElementById('editForm-' + taskId);
+
+                taskLi.classList.remove('hidden');
+                editForm.classList.add('hidden');
+            }
+        </script>
         @endforeach
     </ul>
 </div>
